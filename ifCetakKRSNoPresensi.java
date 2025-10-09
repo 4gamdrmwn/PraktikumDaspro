b69f6834ad721058dcda3e31f00c960ed9fdeabb
Agam Darmawan <madridlover085@gmail.com>
2025-10-09 11:22:41 +0700

    Attempt 1

---
+++ ifCetakKRSNoPresensi.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class ifCetakKRSNoPresensi {
+    // Class implementation goes here
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Print KRS SIAKAD");
+        System.out.println("================");
+        System.out.println("Have the tuiting fee been paid? (yes/no)");
+            boolean uktLunas = sc.nextBoolean();
+            if (uktLunas) {
+                System.out.println("UKT Payment has been confirmed.");
+                System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
+            } else {
+                System.out.println("UKT Payment not confirmed. Please pay the tuition fee first.");
+            }
+    }
+}
