51c973cc1101ac8e6b722c591ba659c35cb7674e
Agam Darmawan <madridlover085@gmail.com>
2025-10-09 11:26:06 +0700

    Attempt 1

--- ifCetakKRSNoPresensi.java
+++ ifCetakKRSNoPresensi.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class ifCetakKRSNoPresensi {
     // Class implementation goes here
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Print KRS SIAKAD");
-        System.out.println("================");
-        System.out.println("Have the tuiting fee been paid? (yes/no)");
-            boolean uktLunas = sc.nextBoolean();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Print KRS SIAKAD");
+            System.out.println("================");
+            System.out.println("Have the tuition fee been paid? (true/false)");
+            boolean uktLunas = scanner.nextBoolean();
             if (uktLunas) {
                 System.out.println("UKT Payment has been confirmed.");
                 System.out.println("KRS now can be printed and you can ask the academic advisor to sign it.");
